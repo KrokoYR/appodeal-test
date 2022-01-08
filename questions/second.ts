b153c815a46bf8cca4497d0138f1a4c40f62b469
Lopson <geekwebs@yandex.com>
2022-01-08 21:47:01 +0300

    Fixed second question answer

--- questions/second.ts
+++ questions/second.ts
@@ -10,11 +10,17 @@ interface IResort {
   holidayId: number | string;
 }
 
-const resorts: IResort[] = [...new Array(1000)]; // just an array;
+type ICalculatedResult = Record<
+  string,
+  {
+    [key: string]: number;
+  }
+>;
+
 const travelMethods = ['train', 'plane'];
 
-const calculatePriceByTravelMethod = (resortsArr: IResort[]) => {
-  const map: Record<string, any> = {};
+export const calculatePriceByTravelMethod = (resortsArr: IResort[]) => {
+  const map: ICalculatedResult = {};
 
   resortsArr.forEach((r) => {
     const { name, price, travelMethod } = r;
@@ -26,13 +32,133 @@ const calculatePriceByTravelMethod = (resortsArr: IResort[]) => {
     const resort = map[name];
 
     if (resort) {
-      resort[travelMethod] = resort[travelMethod]
-        ? resort[travelMethod] + price
-        : 0;
-    } else {
-      map[name] = {};
+      resort[travelMethod] ??= 0;
+      // resort[travelMethod] = resort[travelMethod] ? resort[travelMethod] : 0;
+      resort[travelMethod] += price;
+      return;
     }
+
+    map[name] = {
+      [travelMethod]: price
+    };
   });
 
   return map;
 };
+
+const resortsFixture: IResort[] = [
+  {
+    name: 'string',
+    price: 1000,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'train',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1000,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'train',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1000,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'train',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1000,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'train',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1000,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'train',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1000,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'train',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'not plane',
+    holidayId: 0
+  },
+  {
+    name: 'string',
+    price: 1200,
+    country: 'Random',
+    numberOfDays: 10,
+    travelMethod: 'not train',
+    holidayId: 0
+  }
+];
+
+console.log(calculatePriceByTravelMethod(resortsFixture));
